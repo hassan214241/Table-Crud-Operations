06976cbbe003fee094ec1e4f29049a0bcdcf1334
hassan214241 <hassan214241@gmail.com>
2021-04-30 21:26:40 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './App.css';
+// import Bar from "./component/Bar"
+// import Table from "./component/Table"
+import MiniDrawer from "./component/Task"
+
+
+
+
+function App() {
+return (
+    <>
+    {/* <Bar/>
+   <Table/> */}
+
+   <MiniDrawer/>
+     
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/Task.js
@@ -0,0 +1,251 @@
+
+
+import MaterialTable from 'material-table'
+import clsx from 'clsx';
+import { createStyles, makeStyles, useTheme, Theme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+
+import DashboardIcon from '@material-ui/icons/Dashboard';
+
+import GroupAddIcon from '@material-ui/icons/GroupAdd';
+import MenuBookIcon from '@material-ui/icons/MenuBook';
+
+import ListIcon from '@material-ui/icons/List';
+
+
+
+import React, { useState } from 'react';
+
+
+
+const drawerWidth = 240;
+
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+    },
+    appBar: {
+      zIndex: theme.zIndex.drawer + 1,
+      transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+    },
+    appBarShift: {
+      marginLeft: drawerWidth,
+      width: `calc(100% - ${drawerWidth}px)`,
+      transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    },
+    menuButton: {
+      marginRight: 36,
+    },
+    hide: {
+      display: 'none',
+    },
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0,
+      whiteSpace: 'nowrap',
+    },
+    drawerOpen: {
+      width: drawerWidth,
+      transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    },
+    drawerClose: {
+      transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      overflowX: 'hidden',
+      width: theme.spacing(7) + 1,
+      [theme.breakpoints.up('sm')]: {
+        width: theme.spacing(9) + 1,
+      },
+    },
+    toolbar: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'flex-end',
+      padding: theme.spacing(0, 1),
+      // necessary for content to be below app bar
+      ...theme.mixins.toolbar,
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3),
+    },
+  }),
+);
+
+const empList = [
+    { id: 1, name: "Hassan", email: 'hassan@gmail.com', phone: 923046721523, city: "Gojra" },
+    { id: 2, name: "Umar", email: 'umar@gmail.com', phone: 923045866023, city: "Lahore" },
+    { id: 3, name: "Sajjad", email: 'sajjad342@gmail.com', phone: 923446625523, city: "Faisalabad" },
+    { id: 4, name: "Shahbaz", email: 'shahbaz@gmail.com', phone: 923157855546, city: "Gojra" },
+    { id: 5, name: "Nabeel", email: 'nabeel123@gmail.com', phone: 923455507035, city: "Faisalabad" },
+  ]
+  
+
+export default function MiniDrawer() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  const [data, setData] = useState(empList)
+  const columns = [
+    { title: "ID", field: "id", editable: false },
+    { title: "Name", field: "name" },
+    { title: "Email", field: "email" },
+    { title: "Phone Number", field: 'phone', },
+    { title: "City", field: "city", }
+  ]
+
+
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, {
+              [classes.hide]: open,
+            })}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+          Table Crud Operations
+          </Typography>
+         
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant="permanent"
+        className={clsx(classes.drawer, {
+          [classes.drawerOpen]: open,
+          [classes.drawerClose]: !open,
+        })}
+        classes={{
+          paper: clsx({
+            [classes.drawerOpen]: open,
+            [classes.drawerClose]: !open,
+          }),
+        }}
+      >
+        <div className={classes.toolbar}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          {['Dashboard', 'User', 'Send email', 'News'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <DashboardIcon/> : <MenuBookIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['Menu', 'Trash', 'Logout'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <ListIcon /> : <GroupAddIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+     
+        <div >
+       <hr/>
+      {/* <h4 align="center">Assignment</h4> */}
+      {/* <h4 align='center'>Material Table with CRUD operation</h4> */}
+      <MaterialTable
+        title="Students Data"
+        data={data}
+        columns={columns}
+        editable={{
+          onRowAdd: (newRow) => new Promise((resolve, reject) => {
+            const updatedRows = [...data, { id: Math.floor(Math.random() * 100), ...newRow }]
+            setTimeout(() => {
+              setData(updatedRows)
+              resolve()
+            }, 2000)
+          }),
+          onRowDelete: selectedRow => new Promise((resolve, reject) => {
+            const index = selectedRow.tableData.id;
+            const updatedRows = [...data]
+            updatedRows.splice(index, 1)
+            setTimeout(() => {
+              setData(updatedRows)
+              resolve()
+            }, 2000)
+          }),
+          onRowUpdate:(updatedRow,oldRow)=>new Promise((resolve,reject)=>{
+            const index=oldRow.tableData.id;
+            const updatedRows=[...data]
+            updatedRows[index]=updatedRow
+            setTimeout(() => {
+              setData(updatedRows)
+              resolve()
+            }, 2000)
+          })
+
+        }}
+        options={{
+          actionsColumnIndex: -1, addRowPosition: "first"
+        }}
+      />
+     
+    </div>
+    
+    <hr/>
+    
+    
+      </main>
+    </div>
+  );
+}
