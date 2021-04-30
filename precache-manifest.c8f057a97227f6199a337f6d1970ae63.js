2dd168e9c6ebe5d0a99e9adc6208a66922068943
hassan214241 <hassan214241@gmail.com>
2021-04-30 23:09:29 +0500

    Updates

---
+++ precache-manifest.c8f057a97227f6199a337f6d1970ae63.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36f728d9c4e785b1a70f0665338717a3",
+    "url": "/Table-Crud-Operations/index.html"
+  },
+  {
+    "revision": "58794cc15c48d83d6e8f",
+    "url": "/Table-Crud-Operations/static/css/main.38f3043c.chunk.css"
+  },
+  {
+    "revision": "234959be3404a0ae1a74",
+    "url": "/Table-Crud-Operations/static/js/2.38531241.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/Table-Crud-Operations/static/js/2.38531241.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25736c195485eab85c3a",
+    "url": "/Table-Crud-Operations/static/js/3.f2f35c06.chunk.js"
+  },
+  {
+    "revision": "5f983ef0d998e4957796cf6a040e4dc4",
+    "url": "/Table-Crud-Operations/static/js/3.f2f35c06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4b836e7378dbc499d39",
+    "url": "/Table-Crud-Operations/static/js/4.cdceec71.chunk.js"
+  },
+  {
+    "revision": "312b03fc2032f4a26db5e85de50ce98a",
+    "url": "/Table-Crud-Operations/static/js/4.cdceec71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "310ce344f2f9026298d0",
+    "url": "/Table-Crud-Operations/static/js/5.55dd6d15.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/Table-Crud-Operations/static/js/5.55dd6d15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58794cc15c48d83d6e8f",
+    "url": "/Table-Crud-Operations/static/js/main.3cbd71ba.chunk.js"
+  },
+  {
+    "revision": "26814d8e48f1cd21a326",
+    "url": "/Table-Crud-Operations/static/js/runtime-main.5606db2f.js"
+  }
+]);
